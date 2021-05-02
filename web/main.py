15bd23a7c5516234f8c3e0b61e2beb4530fb0839
musa-42 <83463048+musa-42@users.noreply.github.com>
2021-05-02 13:49:08 +0430

    fix bug

--- web/main.py
+++ web/main.py
@@ -14,12 +14,8 @@ async def main():
     app = aiohttp.web.Application(client_max_size=1024*1024*20)
     app.add_routes([
         aiohttp.web.get('/', server.hello),
-        aiohttp.web.get('/m3u/{peer}', server.grab_m3u),
-        aiohttp.web.get('/watch/{peer}/{mid}/{name}', server.watch_stream),
-        aiohttp.web.get('/watch/{hash}', server.watch_stream),
-        aiohttp.web.get('/w/{h}/{name}', server.watch_stream),
-        aiohttp.web.get('/w/{h}', server.watch_stream),
-        aiohttp.web.get('/watch/{hash}/{name}', server.watch_stream),
+        aiohttp.web.get('/w/{hash}', server.watch_stream),
+        aiohttp.web.get('/w/{hash}/{name}', server.watch_stream),
         aiohttp.web.get('/test_upload', server.test_upload),
         aiohttp.web.post('/upload_big', server.upload_big),
         aiohttp.web.post('/upload', server.upload),
