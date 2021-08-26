b814b83b86c6597ee4141c6cfcd6de07cfacdbe9
sadid998 <75202585+sadid998@users.noreply.github.com>
2021-08-26 16:43:17 +0430

    Update config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -3,14 +3,14 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 1544329
 
-        API_HASH = "......"
+        API_HASH = "e8c801b5516409dcf1549382cb350661"
         
-        BOT_TOKEN = "......."
+        BOT_TOKEN = "1908824339:AAHH4mqV_u8FDdxQ5H9p7vhIBZN5Fly-Brk"
                 
-        channel = 'UserlandApp'
+        channel = 'cinema_great'
         
-        ROOT_URI = f"http://dl1.dlgram.ml"
+        ROOT_URI = "http://dl1.dlgram.ml"
 
         ENCODING = "utf8"
